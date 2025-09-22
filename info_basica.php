682ea7dff2aac1be643671c9af25e236e9a4ceda
Kevin Soliz <kevinsoliz10@gmail.com>
2025-09-22 16:42:08 +0200

    first commit

---
+++ info_basica.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $nombre = "Kevin Soliz";
+    $anyo = 1997;
+    ?>
+    <p>Me llamo <?= $nombre; ?> y nací en el año <?= $anyo; ?>.</p>
+</body>
+</html>
