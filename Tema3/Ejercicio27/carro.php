157e3184ab79fbe9c04308d4fc18e2cff5ab6b21
Kevin Soliz <kevinsoliz10@gmail.com>
2025-11-06 16:38:56 +0100

    Tarea carro

--- Tema3/Ejercicio27/carro.php
+++ Tema3/Ejercicio27/carro.php
@@ -6,11 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-   <li>
-    <ul><a href="">Camiseta</a></ul>
-    <ul><a href="">Libro</a></ul>
-    <ul><a href="">Mesa</a></ul>
-    <ul><a href="">Móvil</a></ul>
-   </li>
+   <ul>
+    <li><a href="?precio=35">Camiseta Element 35€</a></li>
+    <li><a href="?precio=12.30">Libro Crimen y Castigo de F.D. 12.30€</a></li>
+    <li><a href="?precio=499">Mesa Tarsele 499€</a></li>
+    <li><a href="?precio=279">Móvil Balance Phone 279€</a></li>
+</ul>
 </body>
 </html>      
+
+<?php
+
+if(isset($_REQUEST['precio'])){
+   
+}
