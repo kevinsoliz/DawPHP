48a49f41345d353f3785471626e06afcb6a1b48c
Kevin Soliz <kevinsoliz10@gmail.com>
2025-11-07 15:28:46 +0100

    Primera sesion

--- Tema3/Ejercicio27/carro.php
+++ Tema3/Ejercicio27/carro.php
@@ -1,22 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <ul>
-    <li><a href="?precio=35">Camiseta Element 35€</a></li>
-    <li><a href="?precio=12.30">Libro Crimen y Castigo de F.D. 12.30€</a></li>
-    <li><a href="?precio=499">Mesa Tarsele 499€</a></li>
-    <li><a href="?precio=279">Móvil Balance Phone 279€</a></li>
-</ul>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <ul>
+            <li><a href="?precio=35">Camiseta Element 35€</a></li>
+            <li><a href="?precio=12.30">Libro Crimen y Castigo de F.D. 12.30€</a></li>
+            <li><a href="?precio=499">Mesa Tarsele 499€</a></li>
+            <li><a href="?precio=279">Móvil Balance Phone 279€</a></li>
+        </ul>
+        <a href="?vaciarCarro">Vaciar Carro</a>
+        <br>
+        
+    </body>
 </html>      
-
+    
 <?php
 
+session_start();
 if(isset($_REQUEST['precio'])){
+
+   $_SESSION['precio'] = $_REQUEST['precio'];
+   $_SESSION['total'] += $_SESSION['precio'];
+   echo $_SESSION['total'] . ' €';
+
    
+   
+}
+
+if(isset($_REQUEST['vaciarCarro'])){
+    $_SESSION['total'] = 0;
+    echo $_SESSION['total'] . ' €';
+    
 }
