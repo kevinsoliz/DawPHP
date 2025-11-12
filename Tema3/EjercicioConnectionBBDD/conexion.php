4a3011dc7b9ea6078f825d941d4dc9ec66228bf5
Kevin Soliz <kevinsoliz10@gmail.com>
2025-11-12 14:08:05 +0100

    Conexion BBDD exitosa

---
+++ Tema3/EjercicioConnectionBBDD/conexion.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$dbname = 'instituto';
+$username = 'root';
+$password = '';
+
+
+try {
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected to $dbname at $host successfully.";
+
+} catch (PDOException $pe) {
+
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
+
+}
