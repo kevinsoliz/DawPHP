d0712f4a5d59672061afb7acf658992ea57b3b2e
Kevin Soliz <kevinsoliz10@gmail.com>
2025-12-17 18:35:04 +0100

    Ejercicios ajax fáciles, resulta que el preventDefault es necesario en los formularios porque por defecto los submit recargan la página

---
+++ Tema6/Ejercicio2/multiplicar.php
@@ -0,0 +1,6 @@
+<?php
+
+$numero1 = $_POST['num1'];
+$numero2 = $_POST['num2'];
+
+echo $numero1 * $numero2;
---
+++ Tema6/Ejercicio3/mostrar.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$cif = $_POST['cif'];
+$direccion = $_POST['direccion'];
+
+echo "
+    <p> $nombre </p>
+    <p> $cif </p>
+    <p> $direccion </p>
+";
