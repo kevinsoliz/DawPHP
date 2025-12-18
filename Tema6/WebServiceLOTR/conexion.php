2681281b085517780b25c6d578d941737621d647
Kevin Soliz <kevinsoliz10@gmail.com>
2025-12-18 14:42:51 +0100

    la tierra media

---
+++ Tema6/WebServiceLOTR/conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$host = 'localhost';
+$dbname = 'instituto';
+$username = 'root';
+$password = '';
+
+
+try {
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Conexión exitosa a la base de datos." . $dbname;
+} catch (PDOException $pe) {
+
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
+
+}
